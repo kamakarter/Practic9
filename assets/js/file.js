acab6cfbf2438a237a16b76b06d2aa97dd8569fc
kamakarter <160733743+kamakarter@users.noreply.github.com>
2024-09-26 00:50:27 +0300

    Update file.js

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,10 +1,9 @@
 const error = document.querySelector('.error');
-
 const form = document.querySelector('.my-form');
 const nameInput = form.querySelector('.nameForm');
 const dateInput = form.querySelector('.dateForm');
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     error.classList.remove('succes');
@@ -13,40 +12,41 @@ form.addEventListener('submit', (e)=>{
     const name = nameInput.value;
     const date = parseInt(dateInput.value);
 
+    if (!name) {
+        error.innerHTML = 'Поле с именем должно быть заполнено!';
+        nameInput.style.border = 'thin solid red';
+        return;
+    } 
+    
+    else if (name.length < 2) {
+        error.innerHTML = 'Имя должно содержать минимум 2 символа!';
+        nameInput.style.border = 'thin solid red';
+        return;
+    } 
+    else {
+        nameInput.style.border = '';
+    }
+
+    if (!date || isNaN(date)) {
+        error.innerHTML = 'Поле с годом рождения должно быть заполнено!';
+        dateInput.style.border = 'thin solid red';
+        return;
+    } 
+    else {
+        dateInput.style.border = '';
+    }
+
     const currentYear = new Date().getFullYear();
     let ageUser = currentYear - date;
 
-    if (!name && !date){
-        error.innerHTML = 'Проверьте еще раз поля, они пустые!';
-        nameInput.style.border = 'thin solid red';
-        dateInput.style.border = 'thin solid red';
-    }
-    
-    if (!name){
-        error.innerHTML = 'Поле с именем должно быть заполнено!';
-        nameInput.style.border = 'thin solid red';
-    }
-
-    if (name.length < 2){
-        error.innerHTML = 'Имя должно содеражать минимум 2 символа!';
-        nameInput.style.border = 'thin solid red';
-    }
-
-    if (!date){
-        error.innerHTML = 'Поле с годом рождения должно быть заполнено!'
-        dateInput.style.border = 'thin solid red';
-    }
-        
-    if (ageUser <= 18){
+    if (ageUser <= 18) {
         error.innerHTML = 'Должно быть больше 18 лет!';
         dateInput.style.border = 'thin solid red';
-    }
-
-    else {
+    } else {
         dateInput.style.border = 'thin solid green';
-        nameInput.style.border = 'thin solid green';
         error.innerHTML = 'Успешно';
         error.classList.remove('error');
         error.classList.add('succes');
-    }    
-})
+        console.log(name, date);
+    }
+});
